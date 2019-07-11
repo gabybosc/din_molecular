5d472c65c4194e88ba380692c612d2ef1f52a30a
Luz <luli.vercesi@gmail.com>
2019-07-10 23:30:13 -0300

    main para ver como guardar txt

--- src/main.c
+++ src/main.c
@@ -10,31 +10,32 @@
 #include <unistd.h>
 
 #define N 512
-#define L 9.0//cbrt(N/0.8442) //L tiene que ser mayor a 5 para no tener problemas con nuestro radio de corte.
-#define T 0.728
+#define L cbrt(N/0.8442) //L tiene que ser mayor a 5 para no tener problemas con nuestro radio de corte.
+#define T 0.9
 #define H 0.001
 
 int main(){
-  double dL;
-  double *r, *f, *vel, *r_tabla, *r2_tabla, *f_tabla, *V_tabla, fmax;
-  int lines, i, *histograma;
+  double dL, entropia, Epot, Ecin;
+  double *r, *f, *vel, *r_tabla, *r2_tabla, *f_tabla, *V_tabla;
+  int lines, i;
   FILE *fp_tab;
   FILE *fp_en;
-  fp_en = fopen("energias.txt", "a");
-
-  fprintf(fp_en, "N %d, L %f, T %f\n",N,L,T);
-  fprintf(fp_en, "Ecin\tEpot\tEtot\n");
-
   srand(time(NULL));
-
-  int N_verlet = 100;
+  clock_t tic = clock();
+  
+  int N_verlet = 1000;
+  
+  char fn_en[255];
+  sprintf(fn_en, "energiasL%.2fT%.1f.txt",L,T);
+  fp_en = fopen(fn_en, "a");
+  fprintf(fp_en, "N %d, L %f, T %f\n",N,L,T);
+  fprintf(fp_en, "Ecin\tEpot\tH\n");
 
   fp_tab = fopen("tabla_LJ.txt", "r");
   lines = contador_lineas(fp_tab);
 
-  // El formato del filename ".lammpstrj", ese VMD lo lee comodamente
   char fn_vmd[255];
-  sprintf(fn_vmd, "prueba.lammpstrj");
+  sprintf(fn_vmd, "L%.2fT%.1f.lammpstrj",L,T);
 
   r = malloc(3*N * sizeof(double));
   f = malloc(3*N * sizeof(double));
@@ -43,43 +44,25 @@ int main(){
   r2_tabla = malloc((lines-1) * sizeof(double));
   f_tabla = malloc((lines-1) * sizeof(double));
   V_tabla = malloc((lines-1) * sizeof(double));
-  histograma = malloc(2*cbrt(N) * sizeof(int));
 
   leer_tabla(fp_tab, r_tabla, r2_tabla, f_tabla, V_tabla);
 
+  //inicializo
   dL = set_box(r, N, L);
   set_vel(vel, N, T);
-
-
-  printf("Lado = %f\nCONDICIONES INICIALES\n",L);
-  printf("r=%f %f %f\nvel = %f %f %f\nfuerza = %f %f %f\n", *r, *(r+1), *(r+2), *vel, *(vel+1), *(vel+2), *f, *(f+1), *(f+2));
   
-  /*
-  printf("Histograma vx\n");
-  hist(histograma, vel, N, 3, 0);
-  printf("Histograma vy\n");
-  hist(histograma, vel, N, 3, 1);
-  printf("Histograma vz\n");
-  hist(histograma, vel, N, 3, 2);
-*/
   for(i = 0; i < N_verlet; i++){
-    fmax = velocity_verlet(r, vel, f, N, H, L, r2_tabla, f_tabla, V_tabla, lines-1, fp_en);
-    printf("%.1f%%, fmax = %lf\n", 100.0*i/N_verlet, fmax);
-    if(i%10 == 0)
-      save_lammpstrj(fn_vmd, r, vel, N, L, i/10);  // La guardo (append para 0<l)
-  }
+    Epot = velocity_verlet(r, vel, f, N, H, L, r2_tabla, f_tabla, V_tabla, lines-1);
+	if(i%5 == 0){
+	  entropia = teo_h(vel, N);
+	  Ecin = cinetica(vel, N);
+	  fprintf(fp_en, "%lf\t%lf\t%lf\n",Ecin,Epot,entropia);
+	  printf("%d%%\n", 100*i/N_verlet);
+	}
+  }//end verlet
   
-  printf("CONDICIONES FINALES\n");
-  printf("r=%f %f %f\nvel = %f %f %f\nfuerza = %f %f %f\n", *r, *(r+1), *(r+2), *vel, *(vel+1), *(vel+2), *f, *(f+1), *(f+2));
-  printf("\a");
-  /*
-  printf("Histograma vx\n");
-  hist(histograma, vel, N, 3, 0);
-  printf("Histograma vy\n");
-  hist(histograma, vel, N, 3, 1);
-  printf("Histograma vz\n");
-  hist(histograma, vel, N, 3, 2);
-  */
+  save_lammpstrj(fn_vmd, r, vel, N, L, 0); //guardo solo el ultimo frame
+  
   fclose(fp_tab);
   fclose(fp_en);
   free(r);
@@ -89,7 +72,8 @@ int main(){
   free(f_tabla);
   free(r_tabla);
   free(V_tabla);
-  free(histograma);
+  clock_t toc = clock();
+  printf("\nDuracion: %.1f minutos (%.2f horas)\a\n", (double)(toc - tic) / (CLOCKS_PER_SEC*60),(double)(toc - tic) / (CLOCKS_PER_SEC*3600));
 
   return 0;
 }
