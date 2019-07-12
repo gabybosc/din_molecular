af71077815bb68c15896980026faea4ffe028620
Luz <luli.vercesi@gmail.com>
2019-07-12 11:11:24 -0300

    graficador con eje corregido

--- graficador.py
+++ graficador.py
@@ -2,18 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.ion()
 
-datos = np.loadtxt("energiasTs_N125L6.79.txt", skiprows= 2)
+datos = np.loadtxt("res/energiasL8.46T0.73.txt", skiprows= 2)
 
-paso = 5
+paso = 1
 
 ecin = datos[:, 0]
 epot = datos[:, 1]
 etot = ecin+epot
 h = datos[:, 2]
+pres = datos[:, 2]
 t = np.arange(0,paso*len(h),paso)
+Ts = np.arange(2.0,0.35,-0.1)
 
-plt.figure(1)
-
+fig1,ax1 = plt.subplots()
 plt.subplot(3,1,1)
 plt.plot(t,ecin, '.')
 #plt.plot([ecin[0] for i in range(len(ecin))],'r--')
@@ -32,15 +33,14 @@ plt.plot(t,etot, '.')
 plt.plot(t,[etot[0] for i in range(len(etot))],'r--', label="$E_0$")
 plt.ylim([int(etot[0])-1.5,int(etot[0])+0.5])
 plt.ylabel("E*")
-plt.xlabel(r"t* $\times10^{-3}$")
+plt.xlabel("t*")
+plt.text(1.1, -0.1, r"$\times10^{-3}$",horizontalalignment='right',verticalalignment='bottom', transform=ax1.transAxes)
 plt.grid()
 plt.legend()
 
-#plt.savefig("res/energiasL8.46T0.65.png",dpi=150)
-
-plt.figure(2)
+fig2,ax2 = plt.subplots()
 plt.plot(t,h,'.')
-plt.xlabel(r"t* $\times10^{-3}$")
+plt.xlabel("t*")
 plt.ylabel("H")
+plt.text(1.1, -0.1, r"$\times10^{-3}$",horizontalalignment='right',verticalalignment='bottom', transform=ax2.transAxes)
 plt.grid()
-#plt.savefig("res/entropiaL8.46T0.65.png",dpi=150)
